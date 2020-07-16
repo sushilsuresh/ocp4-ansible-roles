3197fda62cb2c88701c71a618eb63b06ee625755
Sushil Suresh <sushil.suresh@gmail.com>
2020-07-16 11:36:21 -0400

    Adding more error handling to prevent a false positive successful run

--- roles/etcd-backup/files/etcd-backup.sh
+++ roles/etcd-backup/files/etcd-backup.sh
@@ -7,8 +7,12 @@ DATE=$(date +%Y%m%dT%H%M%S)
 if [ $? -eq 0 ]; then
     mkdir /etcd-backup/${DATE}
     cp -r /assets/backup/*  /etcd-backup/${DATE}/
-    echo 'Copied backup files to PVC mount point.'
-    exit 0
+    if [ $? -eq 0 ]; then
+    # Making sure we exit clean witout errors only if the cp command is
+    # successful
+        echo 'Copied backup files to PVC mount point.'
+        exit 0
+    fi
 fi
 
 echo "Backup attempts failed. Please FIX !!!"
